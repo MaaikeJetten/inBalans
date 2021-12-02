a5f61cc480545e5f0321c3e45c09e3ba73c22283
nicky-tm <nickytmannetje@gmail.com>
2021-12-02 17:21:35 +0100

    Overlay

---
+++ Wii Project/Assets/Scripts/Overlay/Popup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popup : MonoBehaviour
+{
+    public GameObject popup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        popup.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
