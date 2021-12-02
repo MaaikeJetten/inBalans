c84bf740a16b5a4325aae5e3b470eeaf6d8cdd47
MaaikeJetten <70571425+MaaikeJetten@users.noreply.github.com>
2021-12-02 16:55:55 +0100

    Limitation Character Movement

---
+++ Wii Project/Assets/Scripts/EnableDTScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EnableDTScript : MonoBehaviour
+{
+
+
+    [SerializeField]
+    float eulerAngX;
+    [SerializeField]
+    float eulerAngY;
+    [SerializeField]
+    float eulerAngZ;
+
+
+    void Update()
+    {
+
+        eulerAngX = transform.localEulerAngles.x;
+        eulerAngY = transform.localEulerAngles.y;
+        eulerAngZ = transform.localEulerAngles.z;
+
+    }
+}
+
