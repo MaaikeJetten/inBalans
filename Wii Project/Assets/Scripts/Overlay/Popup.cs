5c72ac063c0927619ad4e747e81a898c29d7b198
nicky-tm <nickytmannetje@gmail.com>
2021-12-03 15:04:32 +0100

    popup programming

--- Wii Project/Assets/Scripts/Overlay/Popup.cs
+++ Wii Project/Assets/Scripts/Overlay/Popup.cs
@@ -6,16 +6,34 @@ using UnityEngine.UI;
 public class Popup : MonoBehaviour
 {
     public GameObject popup;
+    public GameObject uitleg;
+    public GameObject pause;
+    public GameObject success;
+    public GameObject failure;
+
+    public Button begin;
+
+    public string eventTrigger;
 
     // Start is called before the first frame update
     void Start()
     {
-        popup.SetActive(false);
+        popup.SetActive(true);
+        uitleg.SetActive(true);
+        pause.SetActive(false);
+        success.SetActive(false);
+        failure.SetActive(false);
+
+        begin.onClick.AddListener(BeginGame);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void BeginGame() {
+        popup.SetActive(false);
+        eventTrigger = "begin";
     }
 }
